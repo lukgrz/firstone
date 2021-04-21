ac56c230a9d1b880c918773bd15c4567b16cc778
Łukasz Grzegorzak <lukaszgrzegorzak@gmail.com>
2021-04-21 11:03:14 +0200

    7.5 Zadanie: Obliczamy średnią funkcyjnie.

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/arrayOperationsTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.array;
+import com.kodilla.stream.array.ArrayOperations.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+
+public class arrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] theArray = {1,2,3,4,5,6,7,8};
+
+        //When
+        double result = ArrayOperations.getAverage(theArray);
+
+        //Then
+        Assertions.assertEquals(3.5,result);
+
+    }
+}
