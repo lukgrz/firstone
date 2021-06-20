df0b5b4c16e8c0c77e5f4eca625849c3aa269655
Łukasz Grzegorzak <lukaszgrzegorzak@gmail.com>
2021-06-20 16:57:20 +0200

    13.1 Iterate by titles

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.challenges;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        movieStore.printTitles(movieStore.getMovies());
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,43 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public void printTitles (Map<String, List<String>> bookTitles){
+        String book = bookTitles.entrySet().stream()
+                .flatMap(title->title.getValue().stream())
+                .collect(Collectors.joining("!"));
+        System.out.println(book);
+
+        String booktitles = bookTitles.keySet().stream()
+                .map(title->title + ": " + String.join("!", bookTitles.get(title)))
+                .collect(Collectors.joining(" \n"));
+        System.out.println(booktitles);
+    }
+}
