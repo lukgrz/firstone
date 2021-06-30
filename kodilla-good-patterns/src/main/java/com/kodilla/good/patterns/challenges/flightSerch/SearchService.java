5887bfdc3a74c225d6246a904b827efd9815270a
Łukasz Grzegorzak <lukaszgrzegorzak@gmail.com>
2021-06-30 14:19:37 +0200

    13.5 searchFlight update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightSerch/SearchService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightSerch/SearchService.java
@@ -1,31 +1,42 @@
-package com.kodilla.good.patterns.challenges.flightSerch;
+ package com.kodilla.good.patterns.challenges.flightSerch;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SearchService {
 
-    public void flightsFrom(Set<Flight> flights, String city){
-        flights.stream()
+    public Set<Flight> flightsFrom(Set<Flight> flights, String city){
+        Set<Flight>resultFlights = flights.stream()
                 .filter(flight -> flight.getDepartureAirport().getCity().equals(city))
-                .forEach(System.out::println);
+                .collect(Collectors.toSet());
+        return   resultFlights;
     }
 
-    public void flightsTo(Set<Flight> flights, String city) {
-        flights.stream()
+    public Set<Flight> flightsTo(Set<Flight> flights, String city) {
+        Set<Flight>resultFlights = flights.stream()
                 .filter(flight -> flight.getArrivalAirport().getCity().equals(city))
-                .forEach(System.out::println);
+                .collect(Collectors.toSet());
+        return resultFlights;
     }
 
-    public void connectingFlights(Set<Flight> flights, String departureCity, String arrivalCity){
-        flights.stream()
+    public Map<Flight,Set<Flight>> connectingFlights(Set<Flight> flights, String departureCity, String arrivalCity){
+        Map<Flight,Set<Flight>> transitFlights = new HashMap<>();
+
+        Set<Flight>firstFlight = flights.stream()
                 .filter(flight -> flight.getDepartureAirport().getCity().equals(departureCity))
-                .map(flight -> flight.getArrivalAirport().getIataCode())
-                .forEach(transitAirport -> flights.stream()
-                        .filter(flight -> flight.getDepartureAirport().getIataCode().equals(transitAirport))
-                        .filter(flight -> flight.getArrivalAirport().getCity().equals(arrivalCity))
-                        .forEach(System.out::println));
-    }
+                .collect(Collectors.toSet());
 
+        for (Flight flight1:firstFlight) {
+            Set<Flight> secondFlight = flights.stream()
+                    .filter(flight -> flight.getDepartureAirport().getIataCode().equals(flight1.getArrivalAirport().getIataCode()))
+                    .filter(flight -> flight.getArrivalAirport().getCity().equals(arrivalCity))
+                    .collect(Collectors.toSet());
+            if (secondFlight.size()>0) {
+                transitFlights.put(flight1, secondFlight);
+            }
+        }
+        return transitFlights;
+    }
 }
