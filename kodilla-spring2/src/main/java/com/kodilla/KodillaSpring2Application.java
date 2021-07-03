4bdc439771c9fad3142a41326a610562cd8c498c
Łukasz Grzegorzak <lukaszgrzegorzak@gmail.com>
2021-07-03 19:37:16 +0200

    14.1.2 Spring zadanie działające

---
+++ kodilla-spring2/src/main/java/com/kodilla/KodillaSpring2Application.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpring2Application.class, args);
+    }
+
+
+
+}
