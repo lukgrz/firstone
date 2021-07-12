21a81689cb0c61831214d50268acd45c62bb1ec4
lukgrz <lukaszgrzegorzak@gmail.com>
2021-07-12 14:45:48 +0200

    15.1 Project patterns lesson 1

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRuner.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns;
+
+public class PatternsRuner {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 11 - Patterns");
+    }
+}
