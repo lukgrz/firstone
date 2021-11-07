f462476a855e5b4c16404d35ad7eb89c50f3b2c1
lukgrz <lukaszgrzegorzak@gmail.com>
2021-11-07 18:59:08 +0100

    24 Facede shop

--- kodilla-patterns2/src/test/java/com/kodilla/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/facade/ShopServiceTestSuite.java
@@ -1,5 +1,9 @@
 package com.kodilla.facade;
 
+import com.kodilla.facade.api.ItemDto;
+import com.kodilla.facade.api.OrderDto;
+import com.kodilla.facade.api.OrderFacade;
+import com.kodilla.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +14,23 @@ import java.math.BigDecimal;
 class ShopServiceTestSuite {
 
     @Autowired
+    private OrderFacade orderFacade;
+
+    @Autowired
     private ShopService shopService;
+
+    @Test
+    public void testShopFacade() {
+        OrderDto order = new OrderDto();
+        order.addItem(new ItemDto(10L, 2));
+        order.addItem(new ItemDto(216L, 1));
+        order.addItem(new ItemDto(25L, 1));
+        order.addItem(new ItemDto(11L, 3));
+        try {
+            orderFacade.processOrder(order, 1L);
+        } catch (OrderProcessingException e) {
+        }
+    }
 
     @Test
     void testShopServiceSubmitOrder() {
