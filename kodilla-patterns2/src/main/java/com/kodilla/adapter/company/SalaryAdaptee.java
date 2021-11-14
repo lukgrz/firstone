03126672540a85805b38031517ebb40a7b12d232
lukgrz <lukaszgrzegorzak@gmail.com>
2021-11-14 08:23:10 +0100

    24.4 Adapter lesson

---
+++ kodilla-patterns2/src/main/java/com/kodilla/adapter/company/SalaryAdaptee.java
@@ -0,0 +1,18 @@
+package com.kodilla.adapter.company;
+
+import com.kodilla.adapter.company.newhrsystem.CompanySalaryProcessor;
+import com.kodilla.adapter.company.newhrsystem.Employee;
+import com.kodilla.adapter.company.newhrsystem.SalaryProcessor;
+import com.kodilla.adapter.company.oldhrsystem.SalaryCalculator;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SalaryAdaptee implements SalaryProcessor {
+
+    @Override
+    public BigDecimal calculateSalaries(List<Employee> employees) {
+        CompanySalaryProcessor theProcessor = new CompanySalaryProcessor();
+        return theProcessor.calculateSalaries(employees);
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/adapter/company/oldhrsystem/SalaryCalculator.java
@@ -0,0 +1,6 @@
+package com.kodilla.adapter.company.oldhrsystem;
+
+public interface SalaryCalculator {
+
+    double totalSalary(String[][] workers, double[] salaries);
+}
